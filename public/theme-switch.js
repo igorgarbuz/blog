7e8d5d80e9adbf5a3e5301928e69891cebedb906
Igor Garbuz <garbuz.iv@gmail.com>
2024-03-28 13:29:35 +0100

    improvement of theme switch

---
+++ public/theme-switch.js
@@ -0,0 +1,26 @@
+/**
+ * Theme switcher script must have is:inline prop is needed to avoid
+ * flickering of theme colors on page refresh. is:inline prop will exclude script
+ * from the main JS bundle and will load and run it in advance of the page paint.
+ */
+
+const theme = (() => {
+    // Check if user already set a theme
+    const localStorageTheme = localStorage.getItem("theme")
+    if (localStorageTheme) {
+        // User preference over system preference
+        return localStorageTheme
+    }
+    if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+        return "light"
+    }
+    // Default to dark
+    return "dark"
+})()
+
+if (theme === "light") {
+    document.documentElement.classList.add("light")
+} else {
+    document.documentElement.classList.remove("light")
+}
+window.localStorage.setItem("theme", theme)
